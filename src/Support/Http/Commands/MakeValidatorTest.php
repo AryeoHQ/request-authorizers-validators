f0f09568dbd1ee9dd32d54c9d77a17bbd1698ec3
Justin Seliga <justinsel@zillowgroup.com>
2026-03-18 10:01:14 -0400

    chore: upgrade `aryeo/tooling-laravel` to `0.0.31`

--- src/Support/Http/Commands/MakeValidatorTest.php
+++ src/Support/Http/Commands/MakeValidatorTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use Support\Http\Commands\References\Validator;
 use Tests\TestCase;
+use Tooling\GeneratorCommands\References\Contracts\Reference;
 use Tooling\GeneratorCommands\Testing\Concerns\CleansUpGeneratorCommands;
 use Tooling\GeneratorCommands\Testing\Concerns\GeneratesFileTestCases;
 use Tooling\GeneratorCommands\Testing\Concerns\RetrievesNamespaceTestCases;
@@ -23,6 +24,10 @@ final class MakeValidatorTest extends TestCase
         get => new Validator(name: 'TestValidator', baseNamespace: 'Workbench\\App');
     }
 
+    private Reference $nestedReference {
+        get => new Validator(name: 'TestValidator', baseNamespace: 'Workbench\\App\\Nested\\Deeper');
+    }
+
     /** @var array<string, mixed> */
     public array $baselineInput {
         get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App\\'];
@@ -36,6 +41,15 @@ final class MakeValidatorTest extends TestCase
     /** @var array<string, mixed> */
     public array $withoutNamespaceBackslashInput {
         get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App'];
+    }
+
+    /** @var array<string, mixed> */
+    public array $withNestedNamespaceInput {
+        get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App\\Nested\\Deeper'];
+    }
+
+    protected string $expectedNestedFilePath {
+        get => $this->nestedReference->filePath->toString();
     }
 
     #[Test]
