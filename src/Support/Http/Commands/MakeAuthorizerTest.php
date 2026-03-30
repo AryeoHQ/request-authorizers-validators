1b97855d452a84ffc7d2b61ce4351a8c4b674258
Justin Seliga <justinsel@zillowgroup.com>
2026-03-30 13:24:17 -0400

    chore: upgrade `ayreo/tooling-laravel` to `0.0.33` (#45)

--- src/Support/Http/Commands/MakeAuthorizerTest.php
+++ src/Support/Http/Commands/MakeAuthorizerTest.php
@@ -4,33 +4,33 @@ declare(strict_types=1);
 
 namespace Support\Http\Commands;
 
+use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use Support\Http\Commands\References\Authorizer;
 use Tests\TestCase;
+use Tooling\Composer\Composer;
 use Tooling\GeneratorCommands\References\Contracts\Reference;
-use Tooling\GeneratorCommands\Testing\Concerns\CleansUpGeneratorCommands;
 use Tooling\GeneratorCommands\Testing\Concerns\GeneratesFileTestCases;
 use Tooling\GeneratorCommands\Testing\Concerns\RetrievesNamespaceTestCases;
 
 #[CoversClass(MakeAuthorizer::class)]
 final class MakeAuthorizerTest extends TestCase
 {
-    use CleansUpGeneratorCommands;
     use GeneratesFileTestCases;
     use RetrievesNamespaceTestCases;
 
     public Authorizer $reference {
-        get => new Authorizer(name: 'TestAuthorizer', baseNamespace: 'Workbench\\App');
+        get => new Authorizer(name: 'TestAuthorizer', baseNamespace: 'App');
     }
 
     private Reference $nestedReference {
-        get => new Authorizer(name: 'TestAuthorizer', baseNamespace: 'Workbench\\App\\Nested\\Deeper');
+        get => new Authorizer(name: 'TestAuthorizer', baseNamespace: 'App\\Nested\\Deeper');
     }
 
     /** @var array<string, mixed> */
     public array $baselineInput {
-        get => ['name' => 'TestAuthorizer', '--namespace' => 'Workbench\\App\\'];
+        get => ['name' => 'TestAuthorizer', '--namespace' => 'App\\'];
     }
 
     /** @var array<string, mixed> */
@@ -40,12 +40,12 @@ final class MakeAuthorizerTest extends TestCase
 
     /** @var array<string, mixed> */
     public array $withoutNamespaceBackslashInput {
-        get => ['name' => 'TestAuthorizer', '--namespace' => 'Workbench\\App'];
+        get => ['name' => 'TestAuthorizer', '--namespace' => 'App'];
     }
 
     /** @var array<string, mixed> */
     public array $withNestedNamespaceInput {
-        get => ['name' => 'TestAuthorizer', '--namespace' => 'Workbench\\App\\Nested\\Deeper'];
+        get => ['name' => 'TestAuthorizer', '--namespace' => 'App\\Nested\\Deeper'];
     }
 
     protected string $expectedNestedFilePath {
@@ -55,12 +55,16 @@ final class MakeAuthorizerTest extends TestCase
     #[Test]
     public function it_can_make_an_authorizer(): void
     {
+        Composer::fake();
+
         $this->artisan($this->command, $this->baselineInput);
 
-        $this->assertFileExists($this->expectedFilePath, 'The authorizer was not created');
-        $authorizerClass = file_get_contents($this->expectedFilePath);
-        $this->assertStringContainsString('final class TestAuthorizer extends BaseAuthorizer', $authorizerClass, 'The authorizer does not define the class as final');
-        $this->assertStringContainsString('use Support\Http\Authorizer as BaseAuthorizer;', $authorizerClass, 'The authorizer does not import the BaseAuthorizer');
-        $this->assertStringContainsString('extends BaseAuthorizer', $authorizerClass, 'The authorizer does not extend the BaseAuthorizer');
+        $this->assertTrue(File::exists($this->expectedFilePath), 'The authorizer was not created');
+
+        tap(File::get($this->expectedFilePath), function (string $authorizerClass) {
+            $this->assertStringContainsString('final class TestAuthorizer extends BaseAuthorizer', $authorizerClass, 'The authorizer does not define the class as final');
+            $this->assertStringContainsString('use Support\Http\Authorizer as BaseAuthorizer;', $authorizerClass, 'The authorizer does not import the BaseAuthorizer');
+            $this->assertStringContainsString('extends BaseAuthorizer', $authorizerClass, 'The authorizer does not extend the BaseAuthorizer');
+        });
     }
 }
--- src/Support/Http/Commands/MakeValidatorTest.php
+++ src/Support/Http/Commands/MakeValidatorTest.php
@@ -4,33 +4,33 @@ declare(strict_types=1);
 
 namespace Support\Http\Commands;
 
+use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use Support\Http\Commands\References\Validator;
 use Tests\TestCase;
+use Tooling\Composer\Composer;
 use Tooling\GeneratorCommands\References\Contracts\Reference;
-use Tooling\GeneratorCommands\Testing\Concerns\CleansUpGeneratorCommands;
 use Tooling\GeneratorCommands\Testing\Concerns\GeneratesFileTestCases;
 use Tooling\GeneratorCommands\Testing\Concerns\RetrievesNamespaceTestCases;
 
 #[CoversClass(MakeValidator::class)]
 final class MakeValidatorTest extends TestCase
 {
-    use CleansUpGeneratorCommands;
     use GeneratesFileTestCases;
     use RetrievesNamespaceTestCases;
 
     public Validator $reference {
-        get => new Validator(name: 'TestValidator', baseNamespace: 'Workbench\\App');
+        get => new Validator(name: 'TestValidator', baseNamespace: 'App');
     }
 
     private Reference $nestedReference {
-        get => new Validator(name: 'TestValidator', baseNamespace: 'Workbench\\App\\Nested\\Deeper');
+        get => new Validator(name: 'TestValidator', baseNamespace: 'App\\Nested\\Deeper');
     }
 
     /** @var array<string, mixed> */
     public array $baselineInput {
-        get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App\\'];
+        get => ['name' => 'TestValidator', '--namespace' => 'App\\'];
     }
 
     /** @var array<string, mixed> */
@@ -40,12 +40,12 @@ final class MakeValidatorTest extends TestCase
 
     /** @var array<string, mixed> */
     public array $withoutNamespaceBackslashInput {
-        get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App'];
+        get => ['name' => 'TestValidator', '--namespace' => 'App'];
     }
 
     /** @var array<string, mixed> */
     public array $withNestedNamespaceInput {
-        get => ['name' => 'TestValidator', '--namespace' => 'Workbench\\App\\Nested\\Deeper'];
+        get => ['name' => 'TestValidator', '--namespace' => 'App\\Nested\\Deeper'];
     }
 
     protected string $expectedNestedFilePath {
@@ -55,12 +55,16 @@ final class MakeValidatorTest extends TestCase
     #[Test]
     public function it_can_make_a_validator(): void
     {
+        Composer::fake();
+
         $this->artisan($this->command, $this->baselineInput);
 
-        $this->assertFileExists($this->expectedFilePath, 'The validator was not created');
-        $validatorClass = file_get_contents($this->expectedFilePath);
-        $this->assertStringContainsString('final class TestValidator extends BaseValidator', $validatorClass, 'The validator does not define the class as final');
-        $this->assertStringContainsString('use Support\Http\Validator as BaseValidator;', $validatorClass, 'The validator does not import the BaseValidator');
-        $this->assertStringContainsString('extends BaseValidator', $validatorClass, 'The validator does not extend the BaseValidator');
+        $this->assertTrue(File::exists($this->expectedFilePath), 'The validator was not created');
+
+        tap(File::get($this->expectedFilePath), function (string $validatorClass) {
+            $this->assertStringContainsString('final class TestValidator extends BaseValidator', $validatorClass, 'The validator does not define the class as final');
+            $this->assertStringContainsString('use Support\Http\Validator as BaseValidator;', $validatorClass, 'The validator does not import the BaseValidator');
+            $this->assertStringContainsString('extends BaseValidator', $validatorClass, 'The validator does not extend the BaseValidator');
+        });
     }
 }
